33ff411c856dd8448ce206c363614369a3d017f0
Vamsi4724 <Vamsigunjara@gmail.com>
2021-01-30 15:07:35 -0500

    added forgot password design screen

--- app/src/main/java/com/languagelearning/RegistrationActivity.java
+++ app/src/main/java/com/languagelearning/RegistrationActivity.java
@@ -14,6 +14,7 @@ import com.languagelearning.model.ResponseData;
 
 import androidx.appcompat.app.AppCompatActivity;
 import api.EndPointUrl;
+import api.RetrofitInstance;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
