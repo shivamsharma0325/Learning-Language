a33125027ebd21933400e3a26ded788d4ab6dfb6
Nikhil Reddy Buchireddy <bnr6864@gmail.com>
2021-02-11 03:38:25 -0500

    test added and bugs fixed

---
+++ app/src/main/java/com/launguagelearning/AnimalsTestingActivity.java
@@ -0,0 +1,25 @@
+package com.launguagelearning;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AnimalsTestingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_testing);
+        getSupportActionBar().setTitle("Animals - Testing");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
