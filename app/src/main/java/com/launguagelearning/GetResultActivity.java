99ccb7bc0a9b4da3737a3494a851213dd3b4899e
gurpreet400 <gurkhosa400@mail.com>
2021-02-13 13:15:43 -0500

    Results updated

---
+++ app/src/main/java/com/launguagelearning/GetResultActivity.java
@@ -0,0 +1,75 @@
+package com.launguagelearning;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.launguagelearning.api.EndPointUrl;
+import com.launguagelearning.api.RetrofitInstance;
+import com.launguagelearning.model.ResultModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class GetResultActivity extends BaseActivity {
+    ListView list_view;
+    ProgressDialog pd;
+    List<ResultModel> results;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_result);
+        getSupportActionBar().setTitle("Results");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        list_view=(ListView)findViewById(R.id.list_view);
+        results= new ArrayList<>();
+        getResults();
+    }
+
+    private void getResults() {
+        SharedPreferences sharedpreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        pd = new ProgressDialog(GetResultActivity.this);
+        pd.setTitle("Please wait,Data is being loaded...");
+        pd.show();
+        EndPointUrl apiService = RetrofitInstance.getRetrofitInstance().create(EndPointUrl.class);
+        Call<List<ResultModel>> call = apiService.getResults(sharedpreferences.getString("uname","-"));
+        call.enqueue(new Callback<List<ResultModel>>() {
+            @Override
+            public void onResponse(Call<List<ResultModel>> call, Response<List<ResultModel>> response) {
+                pd.dismiss();
+                if (response.code()==200) {
+                    if (response.body() != null) {
+                        if(response.body().size()>0) {
+                            results =response.body();
+                            list_view.setAdapter(new GetResultAdapter(results, GetResultActivity.this));
+                            //Toast.makeText(getApplicationContext(),""+response.body().size(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<List<ResultModel>> call, Throwable t) {
+                pd.dismiss();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
