3b4da61a827e816eb08781cf9cd78837de509b1d
Jageergill <jageer.gill@gmail.com>
2021-02-13 14:00:04 -0800

    Shapes updated

---
+++ app/src/main/java/com/launguagelearning/ShapesTestingActivity.java
@@ -0,0 +1,25 @@
+package com.launguagelearning;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ShapesTestingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shapes_testing);
+        getSupportActionBar().setTitle("Shapes - Testing");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
